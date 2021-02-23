0d2d6b57dbd3e45ad462ec49e0fd4e68025018a2
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-22 23:35:19 -0800

    moved checkForIndexDb to global function to access in online event listenter

--- public/js/indexdb.js
+++ public/js/indexdb.js
@@ -8,34 +8,11 @@ iDBRequest.onupgradeneeded = (event) => {
 
 iDBRequest.onsuccess = (event) => {
     db = event.target.result;
+
+
     if (navigator.onLine) {
         checkForIndexDb();
     }
-    const checkForIndexDb = () => {
-        const transaction = db.transaction(['pending'], 'readwrite');
-        const store = transaction.objectStore('pending');
-        const getData = store.getAll();
-
-        getData.onsuccess = () => {
-            if (getData.result.length > 0) {
-                fetch('/api/transaction/bulk', {
-                    method: 'POST',
-                    body: JSON.stringify(getData.result),
-                    headers: {
-                        Accept: 'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                    }
-                })
-                    .then(data => data.json())
-                    .then(() => {
-                        const transaction = db.transaction(['pending'], 'readwrite');
-                        const store = transaction.objectStore('pending');
-
-                        store.clear();
-                    });
-            }
-        };
-    };
 };
 
 iDBRequest.onerror = (event) => {
@@ -48,3 +25,29 @@ const saveRecord = (record) => {
 
     store.add(record);
 }
+
+const checkForIndexDb = () => {
+    const transaction = db.transaction(['pending'], 'readwrite');
+    const store = transaction.objectStore('pending');
+    const getData = store.getAll();
+
+    getData.onsuccess = () => {
+        if (getData.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getData.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(data => data.json())
+                .then(() => {
+                    const transaction = db.transaction(['pending'], 'readwrite');
+                    const store = transaction.objectStore('pending');
+
+                    store.clear();
+                });
+        }
+    };
+};
